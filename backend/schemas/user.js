53f5f0619673169ad6fcc171912bcf9e67f0e1b8
Alex <al.plamenov@gmail.com>
2022-12-26 13:05:15 +0100

    Schemas Update
    Username & Email Indexing (faster search)
    Email validator (isEmail) mongoose-validator
    Username validator (escape) remove special characters
    User Profile validator(checks if string is URL)
    Added global role field
    Added password reset token & expiry
    Added Communities array for User's joined communities.

--- backend/schemas/user.js
+++ backend/schemas/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
+const validator = require("mongoose-validator");amd 
 
+// Create User Schema
 const userSchema = new mongoose.Schema(
 	{
 		username: {
@@ -8,6 +10,11 @@ const userSchema = new mongoose.Schema(
 			unique: true,
 			trim: true,
 			minlength: 3,
+			index: true,
+			validate: validator({
+				validator: "escape",
+				message: "Invalid characters in username",
+			  }),
 		},
 		password: {
 			type: String,
@@ -20,21 +27,63 @@ const userSchema = new mongoose.Schema(
 			unique: true,
 			trim: true,
 			minlength: 3,
+			index: true,
+			validate: validator({
+				validator: "isEmail",
+				message: "Invalid email format",
+			}),
 		},
+		// Github Account ID
 		githubId: {
 			type: String,
 			unique: true,
 			sparse: true,
 		},
+		// Google Account ID
 		googleId: {
 			type: String,
 			unique: true,
 			sparse: true,
 		},
+		// User Profile Picture
+		profilePicture: {
+			type: String,
+			validate: validator({
+				validator: "isURL",
+				message: "Invalid image URL",
+				protocols: ["http", "https"],
+				require_protocol: true
+			}),
+		},
+		// Array Field for IDs of User's Communities
+		Communities: [{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Community',
+			index: true,
+		}],
+		// User's global role		
+		role: {
+			type: String,
+			required: true,
+			default: "user",
+			enum: ["admin", "moderator", "user"],
+		},
+		// Token issued upon password reset
+		passwordResetToken: {
+			type: String,
+			required: false,
+		},
+		// Expiry date of password reset token
+		passwordResetExpires: {
+			type: Date,
+			required: false,
+		},
 	},
+	// Tells 'createdAt' and 'updatedAt' timestamps
 	{
 		timestamps: true,
 	}
 );
 
+// Export User Model
 module.exports = mongoose.model("User", userSchema);
