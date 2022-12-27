8eb1eea97a300c2228cc332374e4b85c3dc9bb2d
rootz491 <karansh491@gmail.com>
2022-12-28 04:04:18 +0530

    rewrite web sockets with socket.io

--- backend/sockets/websocket.js
+++ backend/sockets/websocket.js
@@ -1,26 +1,40 @@
-const WebSocket = require("ws");
+const { Server } = require("socket.io");
 const logger = require("../utils/logger");
 const storeMessage = require("../database/db.js");
 require("dotenv").config();
 
 const wsport = process.env.WS_PORT;
 
-const wss = new WebSocket.Server({ port: wsport });
-
 function start() {
 	logger.info(`WebSocket-Server listening on port ${wsport}`);
-	wss.on("connection", (ws) => {
-		ws.on("message", async (message) => {
-			logger.info(`Received message: ${message}`);
+
+	const io = new Server(httpServer, {
+		cors: {
+			origin: "*",
+		},
+	});
+
+	io.on("connection", function (socket) {
+		socket.emit("message", "Welcome to the broom server!");
+
+		socket.on("message", async (message) => {
 			try {
 				const parsedMessage = JSON.parse(message);
 				await storeMessage(parsedMessage);
-				ws.send(`Echo: ${message}`);
+				socket.emit("message", "Message received!");
 			} catch (error) {
 				logger.info(error);
 			}
 		});
-		ws.send("Welcome to the WebSocket server!");
+
+		// to emit to all clients
+		socket.on("join", (community) => {
+			console.log(community);
+			io.emit("message", "A new user has joined the community!");
+		});
+
+		// TODO later disconnect
+		// socket.disconnect();
 	});
 }
 
