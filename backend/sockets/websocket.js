0fe458c41bff4fe223fa26a7320278712fb296e1
Alex <al.plamenov@gmail.com>
2022-12-28 14:15:07 +0100

    Websocket.js Update
    Using http module to start server, so can use reverse proxies and middleware
    Added error handling
    Added disconnect listener
    Added JWT auth

--- backend/sockets/websocket.js
+++ backend/sockets/websocket.js
@@ -1,45 +1,93 @@
+const http = require("http");
 const { Server } = require("socket.io");
 const logger = require("../utils/logger");
+const jwt = require("jsonwebtoken");
 const storeMessage = require("../database/db.js");
 require("dotenv").config();
 
-const wsport = process.env.WS_PORT;
+const wsport = process.env.WS_PORT; // WEBSOCKET PORT
+const SECRET_KEY = process.env.JWT_SECRET; // SECRET KEY
 
 function start() {
-	logger.info(`WebSocket-Server listening on port ${wsport}`);
+  logger.info(`WebSocket-Server listening on port ${wsport}`);
 
-	const io = new Server(wsport, {
-		rejectUnauthorized: false,
-		cors: {
-			origin: "*",
-		},
-	});
+  const server = http.createServer();
+  const io = new Server(server, {
+    rejectUnauthorized: false,
+    cors: {
+      origin: "*",
+    },
+  });
 
-	io.on("connection", function (socket) {
-		socket.emit("message", "Welcome to the Broom server!");
+  // add error handling to the connection event
+  io.on("connection", function (socket) {
+    // verify the JWT in the query string
+    const { token } = socket.handshake.query;
+    if (!token) {
+      return socket.disconnect(true); // disconnect the socket if no token is provided
+    }
+    try {
+      // decode and verify with jwt key
+      const payload = jwt.verify(token, SECRET_KEY);
+      // use payload for perms
+      console.log(payload);
+    } catch (error) {
+      return socket.disconnect(true); // disconnect
+    }
 
-		socket.on("message", async (message) => {
-			try {
-				console.log({ message });
+    // Server happy and running
+    socket.emit("message", "Welcome to the Broom server!");
+
+    socket.on("message", async (message) => {
+      try {
+        console.log({ message });
         // store the message in the database
-				await storeMessage(message);
-				// globally echo to all clients
-				io.emit("message", message);
-			} catch (error) {
-				logger.info(error);
-			}
-		});
+        await storeMessage(socket, message);
+        // globally echo to all clients
+        io.emit("message", message);
+      } catch (error) {
+        logger.info(error);
+      }
+    });
 
-		// to emit to all clients
-		socket.on("join", (community) => {
-			console.log({ community });
-			logger.info("A new user has joined the community!");
-			io.emit("message", "A new user has joined the community!");
-		});
+    // Emit to all clients
+    socket.on("join", (community) => {
+      console.log({ community });
+      logger.info("A new user has joined the community!");
+      io.emit("message", "A new user has joined the community!");
+    });
 
-		// TODO later disconnect
-		// socket.disconnect();
-	});
+    // Disconnect function
+    socket.on("disconnect", () => {
+      console.log("Client disconnected");
+      // add logging here later
+    });
+  });
+
+  // error handling
+  server.on("error", (error) => {
+    if (error.syscall !== "listen") {
+      throw error;
+    }
+
+    const bind = typeof wsport === "string" ? "Pipe " + wsport : "Port " + wsport;
+
+    // error handling part 2
+    switch (error.code) {
+      case "EACCES":
+        console.error(bind + " requires elevated privileges");
+        process.exit(1);
+        break;
+      case "EADDRINUSE":
+        console.error(bind + " is already in use");
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  });
+
+  server.listen(wsport);
 }
 
 module.exports = start;
