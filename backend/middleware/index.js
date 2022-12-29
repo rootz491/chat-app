f656099746dbea08d3d4ecd17ff25419f4379764
rootz491 <karansh491@gmail.com>
2022-12-29 23:55:33 +0530

    Fix Authenticated sockets

--- backend/middleware/index.js
+++ backend/middleware/index.js
@@ -1,56 +1,76 @@
 const jwt = require("jsonwebtoken");
+const logger = require("../utils/logger");
 
 exports.isAuthenticated = (req, res, next) => {
 	try {
-	  if (req.headers.authorization) {
-		if (req.headers.authorization.split(" ")[0] !== "Bearer")
-		  throw { message: "Incorrect token prefix", status: 400 };
-		// Check that the JWT token is not empty or undefined
-		const jwtToken = req.headers.authorization.split(" ")[1];
-		if (!jwtToken || jwtToken === "") {
-		  throw { message: "JWT token is empty or undefined" };
-		}
-		jwt.verify(jwtToken, process.env.JWT_SECRET ?? "test", (err, decoded) => {
-		  if (err) {
+		if (req.headers.authorization) {
+			if (req.headers.authorization.split(" ")[0] !== "Bearer")
+				throw { message: "Incorrect token prefix", status: 400 };
+			// Check that the JWT token is not empty or undefined
+			const jwtToken = req.headers.authorization.split(" ")[1];
+			if (!jwtToken || jwtToken === "") {
+				throw { message: "JWT token is empty or undefined" };
+			}
+			jwt.verify(jwtToken, process.env.JWT_SECRET, (err, decoded) => {
+				if (err) {
+					throw {
+						message: "Invalid token",
+						status: 401,
+					};
+				}
+				const { id, github_access_token } = decoded;
+				req.user = {
+					id,
+					github_access_token,
+				};
+				next();
+			});
+		} else {
 			throw {
-			  message: "Invalid token",
-			  status: 401,
+				message: "No token provided",
+				status: 401,
 			};
-		  }
-		  const { id, github_access_token } = decoded;
-		  req.user = {
-			id,
-			github_access_token,
-		  };
-		  next();
-		});
-	  } else {
-		throw {
-		  message: "No token provided",
-		  status: 401,
-		};
-	  }
+		}
 	} catch (error) {
 		if (error.message === "Incorrect token prefix") {
-		  res.status(400).json({
-			message: "Incorrect token prefix",
-		  });
+			res.status(400).json({
+				message: "Incorrect token prefix",
+			});
 		} else if (error.message === "JWT token is empty or undefined") {
-		  res.status(400).json({
-			message: "JWT token is empty or undefined",
-		  });
+			res.status(400).json({
+				message: "JWT token is empty or undefined",
+			});
 		} else if (error.message === "Invalid token") {
-		  res.status(401).json({
-			message: "Invalid token",
-		  });
+			res.status(401).json({
+				message: "Invalid token",
+			});
 		} else if (error.message === "No token provided") {
-		  res.status(401).json({
-			message: "No token provided",
-		  });
+			res.status(401).json({
+				message: "No token provided",
+			});
 		} else {
-		  res.status(500).json({
-			message: "Something went wrong",
-		  });
+			res.status(500).json({
+				message: "Something went wrong",
+			});
 		}
-	  }
-	};
+	}
+};
+
+exports.isSocketAuthenticated = (socket, next) => {
+	try {
+		// verify the JWT in the query string
+		const { token } = socket.handshake.query;
+		if (!token) {
+			logger.info("No token provided");
+			return socket.disconnect(true); // disconnect the socket if no token is provided
+		}
+		// decode and verify with jwt key
+		const payload = jwt.verify(token, process.env.JWT_SECRET);
+		// use payload for perms
+		socket.userInfo = payload;
+		next();
+	} catch (error) {
+		logger.error(error.toString());
+		return socket.disconnect(true); // disconnect
+	}
+};
--- backend/sockets/websocket.js
+++ backend/sockets/websocket.js
@@ -1,100 +1,86 @@
-const http = require("http");
+// const http = require("http");
 const { Server } = require("socket.io");
 const logger = require("../utils/logger");
-const jwt = require("jsonwebtoken");
 const storeMessage = require("../database/db.js");
+const { isSocketAuthenticated } = require("../middleware");
 require("dotenv").config();
 
 const wsport = process.env.WS_PORT; // WEBSOCKET PORT
-const SECRET_KEY = process.env.JWT_SECRET; // SECRET KEY
 
 function start() {
-  logger.info(`WebSocket-Server listening on port ${wsport}`);
+	logger.info(`WebSocket-Server listening on port ${wsport}`);
 
-  const server = http.createServer();
-  const io = new Server(server, {
-    rejectUnauthorized: false,
-    cors: {
-      origin: "*",
-    },
-  });
+	// const server = http.createServer(); //	NOTE:  Not sure why http server is here, so I commented it out for now
+	// const io = new Server(server, {
+	const io = new Server(wsport, {
+		cors: {
+			origin: "*",
+		},
+	});
 
-  // add error handling to the connection event
-  io.on("connection", function (socket) {
-    // verify the JWT in the query string
-    const { token } = socket.handshake.query;
-    if (!token) {
-      return socket.disconnect(true); // disconnect the socket if no token is provided
-    }
-    try {
-      // decode and verify with jwt key
-      const payload = jwt.verify(token, SECRET_KEY);
-      // use payload for perms
-      console.log(payload);
-    } catch (error) {
-      return socket.disconnect(true); // disconnect
-    }
+	// add error handling to the connection event
+	io.use(isSocketAuthenticated).on("connection", function (socket) {
+		// Server happy and running
+		socket.emit("message", "Welcome to the Broom server!");
 
-    // Server happy and running
-    socket.emit("message", "Welcome to the Broom server!");
+		socket.on("message", async (message) => {
+			try {
+				console.log({ message });
+				// store the message in the database
+				await storeMessage(socket, message);
+				// globally echo to all clients
+				io.emit("message", message);
+			} catch (error) {
+				logger.info(error);
+			}
+		});
 
-    socket.on("message", async (message) => {
-      try {
-        console.log({ message });
-        // store the message in the database
-        await storeMessage(socket, message);
-        // globally echo to all clients
-        io.emit("message", message);
-      } catch (error) {
-        logger.info(error);
-      }
-    });
-    
-    socket.on("getInitialMessages", (callback) => {
-      // Get the initial messages from the database
-      const initialMessages = getMessagesFromDatabase();
-      // Send a response with the initial messages
-      callback({ messages: initialMessages });
-    });
-    
-    // Emit to all clients
-    socket.on("join", (community) => {
-      console.log({ community });
-      logger.info("A new user has joined the community!");
-      io.emit("message", "A new user has joined the community!");
-    });
+		socket.on("getInitialMessages", (callback) => {
+			// Get the initial messages from the database
+			const initialMessages = getMessagesFromDatabase();
+			// Send a response with the initial messages
+			callback({ messages: initialMessages });
+		});
 
-    // Disconnect function
-    socket.on("disconnect", () => {
-      console.log("Client disconnected");
-      // add logging here later
-    });
-  });
+		// Emit to all clients
+		socket.on("join", (community) => {
+			console.log({ community });
+			logger.info("A new user has joined the community!");
+			io.emit("message", "A new user has joined the community!");
+		});
 
-  // error handling
-  server.on("error", (error) => {
-    if (error.syscall !== "listen") {
-      throw error;
-    }
+		// Disconnect function
+		socket.on("disconnect", () => {
+			console.log("Client disconnected");
+			// add logging here later
+		});
+	});
 
-    const bind = typeof wsport === "string" ? "Pipe " + wsport : "Port " + wsport;
+	// error handling
+	// server.on("error", (error) => {
+	// 	if (error.syscall !== "listen") {
+	// 		throw error;
+	// 	}
 
-    // error handling part 2
-    switch (error.code) {
-      case "EACCES":
-        console.error(bind + " requires elevated privileges");
-        process.exit(1);
-        break;
-      case "EADDRINUSE":
-        console.error(bind + " is already in use");
-        process.exit(1);
-        break;
-      default:
-        throw error;
-    }
-  });
+	// 	const bind =
+	// 		typeof wsport === "string" ? "Pipe " + wsport : "Port " + wsport;
 
-  server.listen(wsport);
+	// 	// error handling part 2
+	// 	switch (error.code) {
+	// 		case "EACCES":
+	// 			console.error(bind + " requires elevated privileges");
+	// 			process.exit(1);
+	// 			break;
+	// 		case "EADDRINUSE":
+	// 			console.error(bind + " is already in use");
+	// 			process.exit(1);
+	// 			break;
+	// 		default:
+	// 			throw error;
+	// 	}
+	// });
+
+	// server.listen(wsport);
 }
 
 module.exports = start;
