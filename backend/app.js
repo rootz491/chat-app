46870e185ece5b8967bc4c38e3c7ed25bd1c7955
rootz491 <karansh491@gmail.com>
2022-12-30 16:10:12 +0530

    routes stack logger added

--- backend/app.js
+++ backend/app.js
@@ -12,10 +12,9 @@ Sentry.init({
 	dsn: "https://ea69acd8829843d1bae67b685a5e044a@o4504392106770432.ingest.sentry.io/4504392107753472",
 });
 
-mongoose.set("strictQuery", true);
-
 // Connect to MongoDB
 mongoose
+	.set("strictQuery", true)
 	.connect(process.env.MONGO_URI, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
@@ -31,10 +30,10 @@ mongoose
 websocket();
 
 // Routes
-const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const authRouter = require("./routes/auth");
 const { isAuthenticated } = require("./middleware");
+const routerStack = require("./utils/routerStack");
 
 // Express App & Sub-Apps
 const app = express();
@@ -54,7 +53,6 @@ app.use((req, _, next) => {
 app.use(Sentry.Handlers.requestHandler());
 
 // Routes
-app.use("/", indexRouter);
 app.use("/v1/auth", authRouter);
 app.use("/v1/user", isAuthenticated, usersRouter);
 
@@ -64,6 +62,7 @@ app.use(Sentry.Handlers.errorHandler());
 // Express Server
 app.listen(process.env.EXPRESS_PORT, async () => {
 	logger.info("Express-Server started on port 8000");
+	routerStack(app);
 });
 
 //TODO:
